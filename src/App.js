c8eaf4ef9ab372152e6bdc3e8ae799a14aaef765
Sushma Mangalapally <sushmamangalapally@Sushmas-MacBook-Pro.local>
2019-05-18 00:31:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const usersData = [
+        { id: 1, name: 'Lily', username: 'floppydisk'},
+        {id: 2, name: 'Glenn', username: 'silicon'},
+        {id: 3, name: 'Sosa', username: 'university'}
+    ]
+
+    const [users, setUsers] = useState(usersData)
+
+    const addUser = user => {
+        user.id = user.length + 1
+        setUsers([...users, user])
+        /*
+            This function will take a user object
+            as a parameter, and add them to the users
+            array of objects. The ...users code
+            ensures that all the previous users
+            remain in the array.
+        */
+    }
+
+    const deleteUser = id => {
+        setUsers(users.filter(user => user.id !== id))
+    }
+
+    const [editing, setEditing] = useState(false)
+
+    const initialFormState = { id: null, name: '', username: ''}
+
+    const [currentUser, setCurrentUser] = useState(initialFormState)
+
+    const editRow = user => {
+        setEditing(true)
+
+        setCurrentUser({ id: user.id, name: user.name, username: user.username })
+    }
+
+    const updateUser = (id, updatedUser) => {
+        setEditing(false)
+
+        setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+    }
+
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+                    {editing ? (
+                        <div>
+                            <h2>Edit user</h2>
+                            <EditUserForm
+                                editing={editing}
+                                setEditing={setEditing}
+                                currentUser={currentUser}
+                                updateUser={updateUser}
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <h2>Add user</h2>
+                            <AddUserForm addUser={addUser} />
+                        </div>
+                    )}
+                </div>
+                <div className="flex-large">
+                    <h2>View user</h2>
+                    <UserTable users={users} editRow={editRow} deleteUser={deleteUser}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
